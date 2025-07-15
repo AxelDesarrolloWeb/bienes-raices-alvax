b01be38a82a1bd72ae14be5c4db19ab7ba1f1453
Axel Benjamín Alvarez Alcaraz <136202809+AxelDesarrolloWeb@users.noreply.github.com>
2025-07-15 12:36:06 -0300

    admin + seguridad login y bd
    Implementamos autenticación de usuario para que el login del admin tenga un extra de seguridad con un password hasheado. CRUD para las propiedades, ya que el admin puede crear, leer, actualizar y eliminar propiedades... 
    Además de una conexión a nuestra base de datos MySQL para almacenar los datos; con propiedades, usuarios y vendedores como sus respectivas tablas. 
    Ahora dejamos de tener extensiones .html para pasar a .php y reutilizar ciertas partes de nuestro código, eliminando así la redundancia y flexibilizando todo nuestro código. 
    
    Estas y muchas otras funcionalidades fueron agregadas, que esperas, puede probar y revisar el proyecto.

--- src/js/app.js
+++ src/js/app.js
@@ -10,12 +10,26 @@ function darkMode() {
     botonDarkMode.addEventListener('click', function() {
         document.body.classList.toggle('dark-mode');
     });
+
+    const prefiereDarkMode = window.matchMedia('(prefers-color-scheme: dark)');
+    // console.log(prefiereDarkMode.matches);
+    if(prefiereDarkMode.matches) {
+        document.body.classList.add('dark-mode');
+    } else {
+        document.body.classList.remove('dark-mode');
+    }
+
+    prefiereDarkMode.addEventListener('change', function() {
+        if(prefiereDarkMode.matches) {
+            document.body.classList.add('dark-mode');
+        } else {
+            document.body.classList.remove('dark-mode');
+        }
+    });
 }
 function eventListeners() {
     const mobileMenu = document.querySelector('.mobile-menu');
     mobileMenu.addEventListener('click', navegacionResponsive);
-    
-    
 }
 
 function navegacionResponsive() {
